916337e9c3f9f3cad9e104a1fb4217ca9cf02112
Hansamali Wijayatilake <hansamaliwijayatilake@Hansamalis-MacBook-Pro.local>
2022-11-11 14:08:38 +0530

    Add evidence images

--- src/main/java/scripts/dataProvider/FiltertingDataProvider.java
+++ src/main/java/scripts/dataProvider/FiltertingDataProvider.java
@@ -7,8 +7,10 @@ public class FiltertingDataProvider {
 
     @DataProvider(name = "getFilterCriteria")
     public Object[][] getFilterCriteria() {
-            return new Object[][]{{FilteringConditions.USED_CONDITION.getValue(),FilteringConditions.TOYOTA_MANUFACTURER.getValue()
-                    ,FilteringConditions.AXIO_MODEL.getValue(), FilteringConditions.YEAR_2009.getValue(), FilteringConditions.AUTO_TRANSMISSION.getValue()}};
+            return new Object[][]{{FilteringConditions.USED_CONDITION.getValue(),
+                    FilteringConditions.TOYOTA_MANUFACTURER.getValue()
+                    ,FilteringConditions.AXIO_MODEL.getValue(),
+                    FilteringConditions.YEAR_2009.getValue(), FilteringConditions.AUTO_TRANSMISSION.getValue()}};
     }
 
 }
